e96590954f92bff31af3eeb4ff2cf0116d3eca79
DevEmmy <eolaosebikan60@gmail.com>
2023-02-15 11:27:33 -0800

    added sign up

---
+++ pages/sign-in.js
@@ -0,0 +1,16 @@
+// import MetaTag from 'Components/MetaTag'
+import SignIn from '../Components/SignIn'
+import React from 'react'
+import MetaTag from '../Components/MetaTag'
+// import SignIn from '../Components/SignIn'
+
+const signIn = () => {
+  return (
+    <>
+        <MetaTag  title={"Sign In"}/>
+        <SignIn />
+    </>
+  )
+}
+
+export default signIn
---
+++ pages/sign-up.js
@@ -0,0 +1,14 @@
+import MetaTag from '../Components/MetaTag'
+import React from 'react'
+import SignUp from '../Components/SignUp'
+
+const signUp = () => {
+  return (
+    <>
+        <MetaTag title={"Sign Up"} />
+        <SignUp />
+    </>
+  )
+}
+
+export default signUp
